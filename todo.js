698a654545e5c715e8f134ccefecce46142bdd6e
melisaayy <melisa.aydin650@gmail.com>
2024-08-22 22:24:43 +0300

    Todo List

---
+++ todo.js
@@ -0,0 +1,60 @@
+
+
+const inputBox=document.querySelector("#input-box");
+const listContainer=document.querySelector("#list-container");
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must write something");
+    }
+    else{
+        let li =document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";//when we entered value in textbox and click on button,then textbox will resets.
+    saveData();
+};
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName=== "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+// function handleEnter(e){
+//     if(e.key==="Enter"){
+//         addTask();
+       
+//     }
+// }
+// function handleEnter(e){
+//     if(e.code==="Enter"){
+//         addTask();
+       
+//     }
+// }
+inputBox.addEventListener("keypress",function(e){
+    if(e.code==="Enter"){
+        addTask();
+       //When we click enter,then entered value will be save.
+    }
+});
+function saveData(){
+
+    localStorage.setItem("data",listContainer.innerHTML);
+    //the  purpose of writing this code is to prevent information from being lost when the page is refreshed or closed.
+    
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
